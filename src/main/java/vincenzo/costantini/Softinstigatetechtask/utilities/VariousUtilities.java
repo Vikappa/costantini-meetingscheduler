6cba742639aee0d68abdcd71cea997c04e5d8b3c
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-06-28 19:28:01 +0200

    Various utilities service implemented, VariousUtilities validation method implemented to read first line of the input prompt

---
+++ src/main/java/vincenzo/costantini/Softinstigatetechtask/utilities/VariousUtilities.java
@@ -0,0 +1,24 @@
+package vincenzo.costantini.Softinstigatetechtask.utilities;
+
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+
+// Il will keep various parsers and validator here to keep the main classes cleaner
+@Service
+public class VariousUtilities {
+
+    //Checks if the string is in the format YYYY-MM-DD HH:MM:SS EMP###
+    public static boolean validateFirstLineStringFormat(String input) {
+        // Regex pattern for the specified format: YYYY-MM-DD HH:MM:SS EMP###
+        String regex = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} EMP\\d{3}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
+}
+
+}
