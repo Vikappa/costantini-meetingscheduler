966d5dfe1d0f2dc76e0d24cf36341ba0fa6d1836
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-06-28 13:07:40 +0200

    Spring boot initialized into repo, log4j prefix configurated, first hello world run

---
+++ src/test/java/vincenzo/costantini/Softinstigatetechtask/SoftinstigatetechtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzo.costantini.Softinstigatetechtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftinstigatetechtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
