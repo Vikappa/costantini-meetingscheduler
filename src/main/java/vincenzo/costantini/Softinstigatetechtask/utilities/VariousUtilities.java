41485bf24af3a5368cc34b830cf445b8c217b6d9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-06-28 19:38:27 +0200

    second string validation method implemented

--- src/main/java/vincenzo/costantini/Softinstigatetechtask/utilities/VariousUtilities.java
+++ src/main/java/vincenzo/costantini/Softinstigatetechtask/utilities/VariousUtilities.java
@@ -19,6 +19,16 @@ public class VariousUtilities {
         Matcher matcher = pattern.matcher(input);
 
         return matcher.matches();
-}
+    }
+
+    //Checks if the string is in the format YYYY-MM-DD HH:MM d (d is the duration in hours)
+    public static boolean validateSecondLineStringFormat(String input) {
+        String regex = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2} \\d+$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
+    }
 
 }
