663693c5f87e96e558b20cec3d27b7aa58639d59
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-07-01 16:53:36 +0200

    BookingMangerTest boilerplate

---
+++ src/test/java/Softinstigatetechtask/services/BookingManagerTests.java
@@ -0,0 +1,17 @@
+package Softinstigatetechtask.services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookingManagerTests {
+    private static final Logger logger = LoggerFactory.getLogger(BookingManagerTests.class);
+
+    @Test
+    void test1() {
+
+        logger.info("TEST RUNNING HERE");
+        assertTrue(true);
+    }
+}
